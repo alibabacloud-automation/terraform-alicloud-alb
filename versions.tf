b5f2be5346af2d6d5a2c11fbff72cefa56680d39
shanye <shanye.xzq@alibaba-inc.com>
2026-04-17 17:03:21 +0800

    feat!: add provider version constraint for aliyun/alicloud

--- versions.tf
+++ versions.tf
@@ -1,3 +1,15 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0"
+
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = ">= 1.200.0"
+    }
+  }
 }
+
+
+
+
+
